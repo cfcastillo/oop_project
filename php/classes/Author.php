02fb3f7caa6abf2f8bec6b98d8e3e4973a429489
Cecilia Castillo <cecilia.finiello.castillo@gmail.com>
2020-03-16 20:47:33 -0600

    added accessors and mutators - stubs only

--- php/classes/Author.php
+++ php/classes/Author.php
@@ -26,6 +26,15 @@ class Author implements \JsonSerializable {
 	private $authorHash;
 	private $authorUsername;
 
+	/**
+	 * Author constructor.
+	 * @param $newAuthorId
+	 * @param $newAuthorActivationToken
+	 * @param $newAuthorAvatarUrl
+	 * @param $newAuthorEmail
+	 * @param $newAuthorHash
+	 * @param $newAuthorUsername
+	 */
 	public function __construct($newAuthorId, $newAuthorActivationToken, $newAuthorAvatarUrl, $newAuthorEmail, $newAuthorHash, $newAuthorUsername){
 		try{
 			$this->setAuthorId($newAuthorId);
@@ -40,6 +49,107 @@ class Author implements \JsonSerializable {
 		}
 	}
 
+	/**
+	 * accessor method for author id
+	 * @return Uuid value of author id
+	 */
+	public function getAuthorId() : Uuid {
+		return ($this->authorId);
+	}
+
+	/**
+	 * mutator method for author id
+	 * @param Uuid|string $newAuthorId new value of author id
+	 * @throws \TypeError if $newAuthorId is not a uuid
+	 */
+	public function setAuthorId($newAuthorId) : void {
+		return;
+	}
+
+	/**
+	 * accessor method for author activation token
+	 * @return string value of author activation token
+	 */
+	public function getAuthorActivationToken() : string {
+		return ($this->authorActivationToken);
+	}
+
+	/**
+	 * mutator method for author activation token
+	 * @param string $newAuthorActivationToken new value of author activation token
+	 * @throws TODO
+	 */
+	public function setAuthorActivationToken($newAuthorActivationToken) : void {
+		return;
+	}
+
+	/**
+	 * accessor method for author avatar url
+	 * @return string value of author avatar url
+	 */
+	public function getAuthorAvatarUrl() : string {
+		return ($this->authorAvatarUrl);
+	}
+
+	/**
+	 * mutator method for author avatar url
+	 * @param string $newAuthorAvatarUrl new value of author avatar url
+	 * @throws TODO
+	 */
+	public function setAuthorAvatarUrl($newAuthorAvatarUrl) : void {
+		return;
+	}
+
+	/**
+	 * accessor method for author email
+	 * @return string value of author email
+	 */
+	public function getAuthorEmail() : string {
+		return ($this->authorEmail);
+	}
+
+	/**
+	 * mutator method for author email
+	 * @param string $newAuthorEmail new value of author email
+	 * @throws TODO
+	 */
+	public function setAuthorEmail($newAuthorEmail) : void {
+		return;
+	}
+
+	/**
+	 * accessor method for author hash
+	 * @return string value of author hash
+	 */
+	public function getAuthorHash() : string {
+		return ($this->authorHash);
+	}
+
+	/**
+	 * mutator method for author hash
+	 * @param string $newAuthorHash new value of author hash
+	 * @throws TODO
+	 */
+	public function setAuthorHash($newAuthorHash) : void {
+		return;
+	}
+
+	/**
+	 * accessor method for author username
+	 * @return string value of author username
+	 */
+	public function getAuthorUsername() : string {
+		return ($this->authorUsername);
+	}
+
+	/**
+	 * mutator method for author username
+	 * @param string $newAuthorUsername new value of author username
+	 * @throws TODO
+	 */
+	public function setAuthorUsername($newAuthorUsername) : void {
+		return;
+	}
 
 	/**
 	 * formats the state variables for JSON serialization
