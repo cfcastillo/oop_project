76310acd600f6979561c2dcc2ce561568ab85202
Cecilia Castillo <cecilia.finiello.castillo@gmail.com>
2020-04-09 09:09:09 -0600

    fix namespace and remove uuid generator dependency for lecture

--- php/lib/author.php
+++ php/lib/author.php
@@ -1,18 +1,15 @@
 <?php
 require_once(dirname(__DIR__, 1) . "/classes/Author.php");
-require_once(dirname(__DIR__, 1) . "/lib/uuid.php");
+//require_once(dirname(__DIR__, 1) . "/lib/uuid.php");
 
 // The pdo object has been created for you.
 require_once("/etc/apache2/capstone-mysql/Secrets.php");
-$secrets =  new Secrets("/etc/apache2/capstone-mysql/cohort28/cfiniello.ini");
-//$pdo = $secrets->getPdoObject();
+$secrets =  new Secrets("/etc/apache2/capstone-mysql/cohort28/cohort28testing.ini");
+$pdo = $secrets->getPdoObject();
 use CFiniello\ObjectOriented\Author;
 
 //this simulates the front end of a web site. This would actually come from HTML, Javascript, etc.
-$authorId = generateUuidV4(); // "006fb097-31bf-4ead-95a6-717c46e1c7f6";
-
-var_dump($authorId);
-
+$authorId = "006fb097-31bf-4ead-95a6-717c46e1c7f6";
 $authorActivationCode = "006fb09731bf4ead95a6717c46e1c7f6";
 $authorAvatarUrl = "https://avatar.org";
 $authorEmail = "myfakeemail@somewhere.com";
@@ -26,8 +23,8 @@ $authorUsername = "thenewusername3";
 //creating the author object here.
 //runs the __construct
 //runs individual functions.
-//$author =  new Author($authorId, $authorActivationCode, $authorAvatarUrl, $authorEmail, $authorHash, $authorUsername);
-//$author->insert($pdo);
+$author =  new Author($authorId, $authorActivationCode, $authorAvatarUrl, $authorEmail, $authorHash, $authorUsername);
+$author->insert($pdo);
 //$author->update($pdo);
 //$author->delete($pdo);
 
