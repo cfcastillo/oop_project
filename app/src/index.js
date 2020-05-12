28a2e3c83cf7620f1676df4f5c9ac34d8c5bfdf5
Cecilia Castillo <cecilia.finiello.castillo@gmail.com>
2020-05-12 07:42:36 -0600

    added react

---
+++ app/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.css';
+
+const App = () => ( <h1 className="text-info">hello cecilia. today is a great day!</h1> );
+ReactDOM.render(<App/>, document.querySelector('#root'));
